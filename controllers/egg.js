a30e96a6d347391dbf04d06396c70c04712cd894
PrathyushaBavanam <prathyubavanam@gmail.com>
2022-04-20 18:17:55 -0500

    something

--- controllers/egg.js
+++ controllers/egg.js
@@ -37,7 +37,7 @@ exports.egg_create_post = async function(req, res) {
     // {"costume_type":"goat", "cost":12, "size":"large"}
     document.egg_name = req.body.egg_name;
     document.egg_size = req.body.egg_size;
-    document.egg_cost = req.body.egg_cost;
+    document.egg_cost= req.body.egg_cost;
     try{
     let result = await document.save();
     res.send(result);
@@ -48,9 +48,18 @@ exports.egg_create_post = async function(req, res) {
     }
    };
 // Handle egg delete form on DELETE.
-exports.egg_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: egg delete DELETE ' + req.params.id);
-};
+exports.egg_delete = async function(req, res) {
+ //res.send('NOT IMPLEMENTED: egg delete DELETE ' + req.params.id);
+    console.log("delete " + req.params.id)
+    try {
+    result = await egg.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
 // Handle egg update form on PUT.
 exports.egg_update_put = async function(req, res) {
     //res.send('NOT IMPLEMENTED: egg update PUT' + req.params.id);
@@ -62,7 +71,7 @@ ${JSON.stringify(req.body)}`)
  if(req.body.egg_name)
  toUpdate.egg_name = req.body.egg_name;
  if(req.body.egg_size) toUpdate.egg_size = req.body.egg_size;
- if(req.body.egg_cost) toUpdate.egg_cost = req.body.egg_cost;
+ if(req.body.egg_cost) toUpdate.egg_cost= req.body.egg_cost;
  let result = await toUpdate.save();
  console.log("Success " + result)
  res.send(result)
@@ -86,3 +95,59 @@ exports.egg_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
+
+   // Handle a show one view with id specified by query
+exports.egg_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await egg.findById( req.query.id)
+    res.render('eggdetail',
+   { title: 'egg Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+   // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.egg_create_Page = function(req, res) {
+   console.log("create view")
+   try{
+   res.render('eggcreate', { title: 'egg Create'});
+   }
+   catch(err){
+   res.status(500)
+   res.send(`{'error': '${err}'}`);
+   }
+  };
+
+  // Handle building the view for updating a costume.
+// query provides the id
+exports.egg_update_Page = async function(req, res) {
+   console.log("update view for item "+req.query.id)
+   try{
+   let result = await egg.findById(req.query.id)
+   res.render('eggupdate', { title: 'egg Update', toShow: result });
+   }
+   catch(err){
+   res.status(500)
+   res.send(`{'error': '${err}'}`);
+   }
+  };
+
+  // Handle a delete one view with id from query
+exports.egg_delete_Page = async function(req, res) {
+   console.log("Delete view for id " + req.query.id)
+   try{
+   result = await egg.findById(req.query.id)
+   res.render('eggdelete', { title: 'egg Delete', toShow:
+  result });
+   }
+   catch(err){
+   res.status(500)
+   res.send(`{'error': '${err}'}`);
+   }
+  };
--- routes/egg.js
+++ routes/egg.js
@@ -1,6 +1,18 @@
 var express = require('express');
-const egg_controlers= require('../controllers/egg');
+const egg_controllers= require('../controllers/egg');
 var router = express.Router();
 /* GET egg */
-router.get('/', egg_controlers.egg_view_all_Page );
+router.get('/', egg_controllers.egg_view_all_Page );
 module.exports = router;
+
+/* GET detail egg page */
+router.get('/detail', egg_controllers.egg_view_one_Page);
+
+/* GET create costume page */
+router.get('/create', egg_controllers.egg_create_Page);
+
+/* GET create update page */
+router.get('/update', egg_controllers.egg_update_Page);
+
+/* GET delete costume page */
+router.get('/delete', egg_controllers.egg_delete_Page);
